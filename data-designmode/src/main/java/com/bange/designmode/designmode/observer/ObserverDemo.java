32627961cc355993e1a5990826efda443e51e685
longyy <2702045498@qq.com>
2023-03-23 23:34:09 +0800

    更新观察者模式

--- data-designmode/src/main/java/com/bange/designmode/designmode/observer/ObserverDemo.java
+++ data-designmode/src/main/java/com/bange/designmode/designmode/observer/ObserverDemo.java
@@ -6,7 +6,9 @@ public class ObserverDemo {
         WeatherConditions weatherConditions = new WeatherConditions();
         // 数据网站--设置数据并传入接入方WeatherConditions
         WeatherData weatherData = new WeatherData(weatherConditions);
+        // 更新数据方法
         weatherData.setData(18.00f,20.00f,35.0f);
+        System.out.println("==>执行完成");
     }
 
 }
